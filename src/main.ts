ef1fc06c2a90b936d8f3709afdb45bb57acc6e8c
Nicolás Berríos Verboven <nbverboven@gmail.com>
2023-08-16 20:26:57 -0300

    Extract axios requests to a separate file

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import express from 'express';
-import axios from 'axios';
 import {Benefit, Program} from "./utils/types";
-import {mapAirtableBenefits, mapAirtablePrograms} from "./etl/mappers";
+import {fetchBenefits, fetchPrograms} from "./api/requests";
 
 /*
   - fetch programs
@@ -9,13 +8,19 @@ import {mapAirtableBenefits, mapAirtablePrograms} from "./etl/mappers";
   - build internal model
   - make the endpont query the model
 */
-const PROGRAMS_URL = "https://gist.githubusercontent.com/edelbalso/ca099cc11ac46befabcc789c0c905764/raw/0f317591841cdea1e6a1a927dd706c123b9f927f/programs.json";
-const BENEFITS_URL = "https://gist.githubusercontent.com/edelbalso/ca099cc11ac46befabcc789c0c905764/raw/0f317591841cdea1e6a1a927dd706c123b9f927f/benefits.json";
 
 type ProgramsMap = { [id: string]: Program };
 type BenefitsMap = { [id: string]: Benefit };
 
-export const createApp = () => {
+const buildI
+
+export const createApp = async () => {
+    const programs = await fetchPrograms();
+    const programsMap = programs.reduce((acc, curr) => ({...acc, [curr.id]: curr}), {} as ProgramsMap);
+
+    const benefits = await fetchBenefits();
+    //     .reduce((acc, curr) => ({...acc, [curr.id]: curr}), {} as BenefitsMap);
+
     return express()
         .use(express.json())
         .use('/api/incentives', (req, res) => {
@@ -23,27 +28,8 @@ export const createApp = () => {
         });
 };
 
-let benefits = {};
-
 const port = 3000;
 
-(async () => {
-    const programs = await axios.get(PROGRAMS_URL).then(r => mapAirtablePrograms(r.data));
-        // .reduce((acc, curr) => ({...acc, [curr.id]: curr}), {} as ProgramsMap);
-
-    const benefits = await axios.get(BENEFITS_URL).then(r => mapAirtableBenefits(r.data));
-    //     .reduce((acc, curr) => ({...acc, [curr.id]: curr}), {} as BenefitsMap);
-
-    // Object.keys(benefitsMap)
-    //   .map(k => benefitsMap[k])
-    //   .filter(b => b.propertyType === "")
-    //   .map(b => {
-    //     return {
-    //
-    //     }
-    //   });
-
-    createApp().listen(port, () => {
-        console.log(`Starting app listening on port ${port}`);
-    });
-})();
+createApp().then(app => app.listen(port, () => {
+    console.log(`Starting app listening on port ${port}`);
+}));
